12988e7e53a06583703e8900e6a9081647e925e1
WalidBRA <104632116+WalidBRA@users.noreply.github.com>
2023-11-02 15:06:05 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,19 @@
-import "isomorphic-fetch";
-import "materialize-css";
-import "aurelia-bootstrapper";
-import { PLATFORM, Aurelia } from "aurelia-framework";
-// import "materialize-css/dist/css/materialize.css";
+// regenerator-runtime is to support async/await syntax in ESNext.
+// If you don't use async/await, you can remove regenerator-runtime.
+import 'regenerator-runtime/runtime';
+import environment from '../config/environment.json';
+import {PLATFORM} from 'aurelia-pal';
 
-declare const IS_DEV_BUILD: boolean; // The value is supplied by Webpack during the build
+export function configure(aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .feature(PLATFORM.moduleName('resources/index'));
 
-export async function configure(aurelia: Aurelia) {
-	aurelia.use
-		.standardConfiguration()
-		.plugin(PLATFORM.moduleName("aurelia-inputmask"))
-		.plugin(PLATFORM.moduleName("aurelia-materialize-bridge"), plugin => {
-			plugin.useAutoComplete().useBadge().useBreadcrumbs().useBox().useButton().useCard().useCarousel().useCharacterCounter().useCheckbox()
-				.useChip().useCollapsible().useCollection().useColors().useDatePicker().useDropdown().useFab().useFile().useFooter().useInput()
-				.useLookup().useModal().useNavbar()
-				.usePagination().useParallax().useProgress().usePushpin().useRadio().useRange().useScrollSpy().useSelect().useSidenav().useSlider()
-				.useSwitch().useTabs().useTapTarget().useTimePicker().useTooltip().useWaitCursor().useWaves().useWell().useAutoButtonWaves(true)
-				.useValidationContainer();
-		})
-		.plugin(PLATFORM.moduleName("aurelia-validation"))
-		.plugin(PLATFORM.moduleName("aurelia-dialog"));
+  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
 
-	if (IS_DEV_BUILD) {
-		aurelia.use.developmentLogging();
-	}
+  if (environment.testing) {
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
+  }
 
-	aurelia.use.globalResources("shared/collapse-panel");
-	aurelia.use.globalResources("shared/au-markdown");
-	aurelia.use.globalResources("shared/logger");
-	aurelia.use.globalResources("shared/au-code");
-
-	await aurelia.start();
-	// if (document.readyState !== "complete") {
-	// 	await new Promise(resolve => document.addEventListener("DOMContentLoaded", () => resolve()));
-	// }
-	aurelia.setRoot(PLATFORM.moduleName("app"));
+  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
 }
